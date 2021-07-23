29c309c904fe348808b9b6ac306caacf4a314c72
Alison Bernardino <58011286+AlisonBernardino@users.noreply.github.com>
2021-07-23 02:58:48 -0300

    professional entity files added

--- MAJC01Connectivity/src/systemFiles/databaseConfiguration.java
+++ MAJC01Connectivity/src/systemFiles/databaseConfiguration.java
@@ -1,5 +1,7 @@
 package systemFiles;
 
 public class databaseConfiguration {
-    
+    public static String connectionCoordinates = "jdbc:sqlserver://localhost:1433;databaseName=supermarket";
+    public static String databaseUser = "administrator";
+    public static String databasePassword = "admin";
 }
---
+++ MAJC01Connectivity/src/systemParts/Professional.java
@@ -0,0 +1,5 @@
+package systemParts;
+
+public class Professional {
+    
+}
